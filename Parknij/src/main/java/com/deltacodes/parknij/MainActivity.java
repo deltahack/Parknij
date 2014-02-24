af48aaea13002f8f4875393ce9928306a41d7056
Piotrek Janiak <janiak.piotrek@gmail.com>
2014-02-24 14:50:04 +0100

    Działająca mapa

--- Parknij/src/main/java/com/deltacodes/parknij/MainActivity.java
+++ Parknij/src/main/java/com/deltacodes/parknij/MainActivity.java
@@ -1,15 +1,14 @@
 package com.deltacodes.parknij;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import com.google.android.gms.maps.MapFragment;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -28,7 +27,7 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
